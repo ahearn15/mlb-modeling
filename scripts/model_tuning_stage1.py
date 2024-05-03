d7c66dd71f299b136cb7724fff5841e17f68a2a5
Adam Hearn <55107180+ahearn15@users.noreply.github.com>
2024-05-03 15:33:20 -0400

    update n splits

--- scripts/model_tuning_stage1.py
+++ scripts/model_tuning_stage1.py
@@ -302,7 +302,7 @@ def fit_kfold(model, n_splits, n_search):
 
 
 def model_evaluation(model):
-    scores = fit_kfold(model, n_splits=5, n_search=2)
+    scores = fit_kfold(model, n_splits=10, n_search=1000)
     return scores
 
 
